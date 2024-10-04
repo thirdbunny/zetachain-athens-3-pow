9125097fcd190f734437433ff8cfb39b65400290
Third Bunny Official <thirdbunny.xyz@gmail.com>
2024-10-04 16:26:32 +0000

    update script

--- push-log.sh
+++ push-log.sh
@@ -2,24 +2,27 @@ log_file="$HOME/node_height_log.txt"
 template_file="./TEMPLATE.md"
 output_file="README.md"
 
-# Read the log file
-log_content=$(cat "$log_file")
+
+while true; do
+  # Read the log file
+  log_content=$(cat "$log_file")
 
 
-# Replace the placeholder "_____" with the content of node_height_log.txt
-cp $template_file $output_file	
-echo "\`\`\`"  >> $output_file
-echo "$log_content" >> $output_file
-echo "\`\`\`"  >> $output_file
+  # Replace the placeholder "_____" with the content of node_height_log.txt
+  cp $template_file $output_file
+  echo "\`\`\`"  >> $output_file
+  echo "$log_content" >> $output_file
+  echo "\`\`\`"  >> $output_file
 
-# Add changes to Git
-git add "$output_file"
+  # Add changes to Git
+  git add "$output_file"
 
 
-# Commit the changes
-git commit -m "Updated node height logs"
+  # Commit the changes
+  git commit -m "Updated node height logs"
 
-# Push the changes to GitHub
-git push origin main
-
+  # Push the changes to GitHub
+  git push origin main
+  sleep 60
+done
 
